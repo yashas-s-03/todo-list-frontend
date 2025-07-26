5471f1d1360527b29495f3d9fda380fdd4da9828
yashas-s-03 <rabc211@gmail.com>
2025-07-27 01:32:01 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-// to-do-list/vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+  // to-do-list/vite.config.js
+  import { defineConfig } from 'vite';
+  import react from '@vitejs/plugin-react';
+  import path from 'path';
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: '../backend/client',  // output React build into backend/client
-    emptyOutDir: true,
-  },
-});
+  export default defineConfig({
+    plugins: [react()],
+    build: {
+      outDir: '../backend/client',  // output React build into backend/client
+      emptyOutDir: true,
+    },
+  });
